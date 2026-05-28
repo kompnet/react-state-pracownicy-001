86075a35ce59442f6d11e5856e9d52f60524b640
Grzegorz Kwiesielewicz <kompnet@gmail.com>
2026-05-28 07:31:04 +0200

    usuwanie pracownika

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const pracownicy = [
+    {
+    id: 1,
+    imie: 'Adam',
+    nazwisko: 'Kowalski',
+    pensja: 15000
+  }, 
+  {
+    id: 2,
+    imie: 'Jan',
+    nazwisko: 'Nowak',
+    pensja: 10000
+  },
+  {
+    id: 3,
+    imie: 'Ewa',
+    nazwisko: 'Markowska',
+    pensja: 25000
+  }
+
+
+]
+  const usunPracownika = (id) => {
+    const nowaLista = lista.filter( pracownik => pracownik.id !== id)
+    setLista(nowaLista)
+  }
+
+  const [lista, setLista] = useState(pracownicy)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <ul>
+      {
+        lista.map( pracownik => (
+          <li key={pracownik.id}>{pracownik.imie} <button onClick={() => (usunPracownika(pracownik.id))}>Usuń</button></li>
+
+        ))
+      }
+    </ul>
+  
+  </>
   );
 }
 
